8c6e0c9657c053d93c8b7db0642efd035b5e8e95
root <root@hostal.elfos.cl>
2016-07-20 13:31:20 -0400

    Wrong website path in controller

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -5,5 +5,5 @@ class website_gallery(http.Controller):
         @http.route(['/page/website.photogallery', '/page/photogallery'], type='http', auth="public", website=True)
         def rooms(self, **kw):
                 Photos = http.request.env['website.photogallery']
-                return http.request.render("website.photo_gallery", {'photos': Photos.search([]) })
+                return http.request.render("website.photogallery", {'photos': Photos.search([]) })
 
