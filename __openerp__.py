44d2b7daaf73d655568a58fe1dca6293b4504371
hallos <oscar.hall@outlook.com>
2016-07-22 10:27:55 -0400

    Add public access rights to photogallery

--- __openerp__.py
+++ __openerp__.py
@@ -3,6 +3,8 @@
         'description': 'Adds a Photo Gallery page to website where the admin can add photos from the backend',
         'author': 'Oscar Hall',
         'depends': ['website'],
-        'data': ['views/website_photogallery_view.xml', 'views/website_photogallery_frontend.xml'],
+        'data': ['views/website_photogallery_view.xml', 
+				'views/website_photogallery_frontend.xml',
+				'security/ir.amodel.access.csv'],
 }
 
